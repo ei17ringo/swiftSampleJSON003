4798b48c6876139b79632bb2db23daffc473cb70
ei17ringo <ei17ringo@gmail.com>
2016-02-03 19:30:15 +0800

    commit init

--- sampleJSON003/ViewController.swift
+++ sampleJSON003/ViewController.swift
@@ -14,6 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        //-- json.txtファイルを読み込んで
+        let path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+        let jsondata = NSData(contentsOfFile: path!)
+        //-- 辞書データに変換して
+        let jsonDictionary = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSDictionary
+        //--  辞書データの個数だけ繰り返して表示する
+        for (key,data) in jsonDictionary {
+            var d1 = data["餅"] as! String
+            var d2 = data["醤油"] as! String
+            var d3 = data["月見団子"] as! String
+
+            
+            print("キー[\(key)] 餅[\(d1)]")
+            print("キー[\(key)] 醤油[\(d2)]")
+            print("キー[\(key)] 月見団子[\(d3)]")
+        }
+
+        
+        
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
